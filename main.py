cad4e8605968b80713a597f10ba7a171a044b91d
YangTh599 <YangTh599@northwested.org>
2024-10-08 10:53:51 -0400

    get number function

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+# Thayer Yang
+# 08 OCT 2024
+# Basic Calculator
+
+def getNumber(message):
+    number = 0
+    while True:
+        try:
+            number = float(input(message))
+            return number
+        except ValueError:
+            print("Invalid number.")
+
+
 print("""Hello, welcome to my Basic Calculator!
 This script will prompt you to enter two numbers
 and then add, subract, multiply, or divide the numbers
@@ -9,28 +23,28 @@ Choose a math operation to perform:
     3. Multiplication (*)
     4. Division (/)""")
 
-option = 0
+operation = 0
 
 while True:
-    option = int(input("Enter your choice (1-4): "))
-    if option >=1 and option <=4:
+    operation = int(getNumber("Enter your choice (1-4): "))
+    if operation >=1 and operation <=4:
         break
     else:
         print("Invalid input.")
 
-num1 = float(input("Please enter your first number: "))
-num2 = float(input("Please enter your second number: "))
+num1 = getNumber("Please enter your first number: ")
+num2 = getNumber("Please enter your second number: ")
 
-if option == 1:
+if operation == 1:
     answer = num1 + num2
     print(f'{num1} plus {num2} equals: {answer}')
-elif option == 2:
+elif operation == 2:
     answer = num1 - num2
     print(f'{num1} minus {num2} equals: {answer}')
-elif option ==3:
+elif operation ==3:
     answer = num1*num2
     print(f'{num1} times {num2} equals: {answer}')
-elif option ==4:
+elif operation ==4:
     if num2 == 0:
         print("ERROR: Cannot divide by zero.")
     else:
